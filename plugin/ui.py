a2e3284e2e921c95530e8f2778f0f7d8eac7a135
Cokeaddict <motosportm3@gmail.com>
2013-11-09 01:55:34 +0000

    Fixed Menu button

--- plugin/ui.py
+++ plugin/ui.py
@@ -119,7 +119,7 @@ class OnDemand_Screen(Screen, ConfigListScreen):
 		self["arrowup"]   = MultiPixmap()
 		self["arrowdown"]   = MultiPixmap()
 
-		self["actions"]  = ActionMap(["SetupActions", "TimerEditActions"], {
+		self["actions"]  = ActionMap(["OkCancelActions", "ShortcutActions", "WizardActions", "ColorActions", "SetupActions", "NumberActions", "MenuActions", "HelpActions"], {
 			"ok"    : self.keyOK,
 			"cancel": self.keyCancel,
 			"menu" : self.keySetup,
